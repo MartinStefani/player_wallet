f964028fe8460a66b380d42b4f22860e8b43f9d9
Martin Stefani <martin@tekita.si>
2020-07-08 02:13:33 +0200

    Wip: API endpoints set, but an error is showing up

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -30,17 +30,99 @@ export const register = (app: express.Application) => {
         }
     });
 
+    app.post('/api/play-session', async (req: any, res) => {
+        try {
+            const playerId = parseInt(req.body.playerId, 10);
+            const playSessionName = req.body.betAmount || '';
+            const betFactor = parseFloat(req.body.betFactor);
+
+            const playSessionId = await db.one(`
+                INSERT INTO play_session (player_id, play_session_name, bet_factor)
+                     VALUES ($[playerId], $[playSessionName], $[betFactor])
+                  RETURNING play_session_id;
+            `, { playerId, playSessionName, betFactor });
+
+            return res.json({ playSessionId });
+        } catch (err) {
+            res.json({ error: err.message || err });
+        }
+    });
+
+    app.post('/api/bet', async (req: any, res) => {
+        try {
+            const playSessionId = parseInt(req.body.playSessionId, 10);
+            const betAmount = parseFloat(req.body.amount);
+
+            const betId = await db.one(`
+                UPDATE play_session
+                   SET bet_amount = $[betAmount]
+                 WHERE play_session_id = $[playSessionId]
+            `, { playSessionId, betAmount });
+
+            return res.json({ playSessionId });
+        } catch (err) {
+            res.json({ error: err.message || err });
+        }
+    });
+
+    // Win a bet, a transaction is created and the amount is added to the player.wallet_fund = wallet_fund + betAmount * betFactor
+    app.post('/api/win', async (req: any, res) => {
+        try {
+            const playSessionId = parseInt(req.body.playSessionId, 10);
+            const sessionClosingType = 'won';
+            const resultCode = 'out';
+            const procOutput = await db.proc('play_session_close', { playSessionId, sessionClosingType, resultCode });
+
+            return res.json({ code: procOutput.resultcode });
+        } catch (err) {
+            res.json({ error: err.message || err });
+        }
+    });
+
+    // Lose a bet, no transaction is created, just the session is closed
+    app.post('/api/lose', async (req: any, res) => {
+        try {
+            const playSessionId = parseInt(req.body.playSessionId, 10);
+            const sessionClosingType = 'lost';
+            const resultCode = 'out';
+            const procOutput = await db.proc('play_session_close', { playSessionId, sessionClosingType, resultCode });
+
+            return res.json({ code: procOutput.resultcode });
+        } catch (err) {
+            res.json({ error: err.message || err });
+        }
+    });
 
     app.get('/api/history/:playerId', async (req: any, res) => {
         try {
             const playerId = parseInt(req.params.playerId, 10);
-            const history = await db.any(`
-                SELECT player_id
-                  FROM player
-                 WHERE player_id = $[playerId]
-                ORDER BY player_id`, { playerId });
+            const playerHistory = await db.any(`
+                SELECT p.player_id, p.player_name, p.wallet_funds, p.created
+                  FROM player AS p
+                 WHERE p.player_id = $[playerId];`, { playerId });
 
-            return res.json(history);
+            const walletTransactionHistory = await db.any(`
+                SELECT wt.wallet_transaction_id,
+                       wt.play_session_id,
+                       wt.wallet_funds_before,
+                       wt.transaction_amount,
+                       wt.created
+                  FROM wallet_transaction AS wt
+                 WHERE wt.player_id = $[playerId]
+                 ORDER BY wt.created ASC;`, { playerId });
+
+            const playSessionHistory = await db.any(`
+                SELECT ps.play_session_id,
+                       ps.play_session_name,
+                       ps.session_status,
+                       ps.bet_amount,
+                       ps.bet_factor,
+                       ps.created,
+                       ps.last_updated
+                  FROM play_session AS ps
+                 WHERE ps.player_id = $[playerId];`, { playerId });
+
+            return res.json({ player: playerHistory, wallet_transactions: walletTransactionHistory, play_sessions: playSessionHistory});
         } catch (err) {
             res.json({ code: err.message || err });
         }
--- tools/initdb.pgsql
+++ tools/initdb.pgsql
@@ -50,13 +50,14 @@ ALTER TABLE public.player
 
 CREATE TABLE public.play_session
 (
-    play_session_id integer NOT NULL,
+    play_session_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     player_id integer NOT NULL,
     bet_amount numeric(8,2) NOT NULL DEFAULT 0,
     bet_factor numeric(8,2) NOT NULL DEFAULT 1,
-    created timestamp without time zone NOT NULL,
+    created timestamp without time zone NOT NULL DEFAULT now(),
     last_updated timestamp without time zone NOT NULL DEFAULT now(),
-    session_status session_status NOT NULL DEFAULT 'open'::session_status,
+    play_session_name character varying(50) COLLATE pg_catalog."default",
+    session_status character varying(10) COLLATE pg_catalog."default" NOT NULL DEFAULT 'open'::character varying,
     CONSTRAINT play_session_pkey PRIMARY KEY (play_session_id)
 )
 
@@ -65,29 +66,31 @@ TABLESPACE pg_default;
 ALTER TABLE public.play_session
     OWNER to postgres;
 
+-- Play_session - End
+-- ===========================================================================
+-- wallet_transaction - Start
+
 -- Table: public.wallet_transaction
 
 -- DROP TABLE public.wallet_transaction;
 
 CREATE TABLE public.wallet_transaction
 (
-    wallet_transaction_id integer NOT NULL,
+    wallet_transaction_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     player_id integer NOT NULL,
     play_session_id integer NOT NULL,
     wallet_funds_before numeric(8,2) NOT NULL,
     transaction_amount numeric(8,2) NOT NULL,
-    created timestamp without time zone NOT NULL,
+    created timestamp without time zone NOT NULL DEFAULT now(),
     CONSTRAINT wallet_transaction_pkey PRIMARY KEY (wallet_transaction_id),
     CONSTRAINT fk_play_session_id FOREIGN KEY (play_session_id)
         REFERENCES public.play_session (play_session_id) MATCH SIMPLE
         ON UPDATE RESTRICT
-        ON DELETE RESTRICT
-        NOT VALID,
+        ON DELETE RESTRICT,
     CONSTRAINT fk_player_id FOREIGN KEY (player_id)
         REFERENCES public.player (player_id) MATCH SIMPLE
         ON UPDATE RESTRICT
         ON DELETE RESTRICT
-        NOT VALID
 )
 
 TABLESPACE pg_default;
@@ -111,9 +114,70 @@ CREATE INDEX fki_fk_player_id
     (player_id ASC NULLS LAST)
     TABLESPACE pg_default;
 
+-- wallet_transaction - end
+-- ===========================================================================
 
 -- Preexisting data
 INSERT INTO player (player_name, wallet_funds)
      VALUES ('Aldo Baio', 0);
 
 
+-- Procedures
+
+CREATE OR REPLACE PROCEDURE public.play_session_close(
+	playsessionid integer,
+	closingstatus character varying,
+	INOUT resultcode character varying)
+LANGUAGE 'plpgsql'
+AS $BODY$
+	DECLARE 
+		currentSessionStatus VARCHAR(50);
+		betAmount NUMERIC(8,2);
+		betFactor NUMERIC(8,2);
+		walletFundsBefore NUMERIC(8,2);
+		transactionAmount NUMERIC(8,2);
+		playerId INT;
+BEGIN 
+	resultCode := 'UNDEFINED';
+
+	SELECT ps.session_status, ps.bet_amount, ps.bet_factor, p.wallet_funds, p.player_id
+	  INTO currentSessionStatus, betAmount, betFactor, walletFundsBefore, playerId
+	  FROM player AS p
+	 INNER JOIN play_session AS ps
+			 ON p.player_id = ps.player_id
+	 WHERE ps.play_session_id = playSessionId;
+
+	IF (currentSessionStatus <> 'open') THEN
+	    resultCode := 'BET_NOT_OPEN';
+		RETURN;
+	END IF;
+
+	IF (closingStatus = 'won') THEN
+	    transactionAmount := betAmount * betFactor;
+	
+		-- Transaction: begin //TODO
+		
+		UPDATE play_session
+		   SET session_status = closingStatus
+		 WHERE play_session_id = playSessionId;
+		 
+		INSERT INTO wallet_transaction(player_id, play_session_id, wallet_funds_before, transaction_amount)
+		     VALUES (playerId, playSessionId, walletFundsBefore, transactionAmount);
+			 
+	    UPDATE player
+		   SET wallet_funds = wallet_funds + transactionAmount
+		 WHERE player_id = playerId;
+		 
+		 -- Transaction: commit //TODO
+		 
+		 resultCode := 'BET_WON';
+	ELSEIF (closingStatus = 'lost') THEN
+		UPDATE play_session
+		   SET session_status = closingStatus,
+               last_updated = now()
+		 WHERE play_session_id = playSessionId;
+		
+		-- No wallet_transactions
+	END IF;
+END;
+$BODY$;
